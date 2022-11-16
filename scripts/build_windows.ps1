ea3fc547ee7a061e2ea7941eaebf53c1e3a160dd
LemonPi314 <49930425+LemonPi314@users.noreply.github.com>
2022-11-15 23:47:24 -0500

    Update windows build script

--- scripts/build_windows.ps1
+++ scripts/build_windows.ps1
@@ -1,61 +1,71 @@
 # Save original working directory.
 $OriginalLocation = Get-Location
 
-# Find the parent directory containing the setup.py file.
-Write-Host "Looking for setup.py..."
-while ($True)
+try
 {
-    if (Test-Path "setup.py")
+    # Find the parent directory containing the setup.py file.
+    Write-Host "Looking for setup.py..."
+    while ($True)
     {
-        $Root = Get-Location
-        break
+        if (Test-Path "setup.py")
+        {
+            $BaseDirectory = Get-Location
+            break
+        }
+        elseif ((Get-Location).Path -eq "$(Split-Path $PSScriptRoot -Qualifier)\")
+        {
+            Write-Host "setup.py not found."
+            exit
+        }
+        else
+        {
+            Set-Location ".."
+        }
     }
-    else
+
+    # Create a venv if one doesn't exist.
+    if (-not (Test-Path ".venv"))
     {
-        Set-Location ".."
+        Write-Host "Creating virtual environment..."
+        python -m venv .venv
     }
-}
 
-# Create a venv if one doesn't exist.
-if (-not (Test-Path ".venv"))
+    # Activate the venv.
+    . ".venv\Scripts\Activate.ps1"
+
+    # Install build dependencies.
+    Write-Host "Installing build dependencies..."
+    pip install -r requirements-dev.txt
+
+    # If not already present, download the AutoTrace repository.
+    if (-not (Test-Path "third-party"))
+    {
+        Write-Host "Downloading AutoTrace repository..."
+        New-Item "third-party" -ItemType Directory | Out-Null
+        Set-Location "third-party"
+        git clone https://github.com/autotrace/autotrace.git
+        Set-Location "autotrace"
+        git reset --hard fcd9043f6227979ea2b21ac5d9f796325bdb1343
+        Set-Location "distribute\win\3rdparty"
+        Expand-Archive "glib-dev_2.34.3-1_win64.zip" -DestinationPath "glib"
+        Set-Location $BaseDirectory
+    }
+
+    # Clean build files.
+    Write-Host "Cleaning build files..."
+    python setup.py clean --all
+    if (Test-Path "build")
+    {
+        Remove-Item "build" -Recurse
+    }
+
+    # Build distributions.
+    Write-Host "Building distributions..."
+    python setup.py sdist
+    python setup.py bdist_wheel
+}
+finally
 {
-    Write-Host "Creating virtual environment..."
-    python -m venv .venv
+    # Restore original working directory.
+    Set-Location $OriginalLocation
 }
-
-# Activate the venv.
-. ".venv\Scripts\Activate.ps1"
-
-# Install build dependencies.
-Write-Host "Installing build dependencies..."
-pip install -r requirements-dev.txt
-
-# If not already present, download the AutoTrace repository.
-if (-not (Test-Path "third-party"))
-{
-    Write-Host "Downloading AutoTrace repository..."
-    New-Item "third-party" -ItemType Directory
-    Set-Location "third-party"
-    git clone https://github.com/autotrace/autotrace.git
-    Set-Location "autotrace"
-    git reset --hard fcd9043f6227979ea2b21ac5d9f796325bdb1343
-    Set-Location "distribute\win\3rdparty"
-    Expand-Archive "glib-dev_2.34.3-1_win64.zip" -DestinationPath "glib"
-    Set-Location $Root
-}
-
-# Clean build files.
-Write-Host "Cleaning build files..."
-python setup.py clean --all
-if (Test-Path "build")
-{
-    Remove-Item "build" -Recurse
-}
-
-# Build distributions.
-Write-Host "Building distributions..."
-python setup.py sdist
-python setup.py bdist_wheel
-
-# Restore original working directory.
-Set-Location $OriginalLocation
